8b7aa72308858957bd5cc82afcfdebeb6c79332a
Bryce Bergmann <bryce.bergmann_2014@outlook.com>
2026-01-06 21:51:10 -0600

    add homepage UI

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-export async function getServerSideProps() {
-  return {
-    redirect: {
-      destination: "/map",
-      permanent: false,
-    },
-  };
-}
-
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <h1>ArchaeoRest</h1>
+      <p>Welcome</p>
+      <a href="/map">Go to map</a>
+    </div>
+  );
 }
