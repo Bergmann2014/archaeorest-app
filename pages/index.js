f69275b2c57f9bd3375a594656e24f5931705839
Bryce Bergmann <bryce.bergmann_2014@outlook.com>
2026-01-06 22:00:24 -0600

    redirect home to /map

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/map",
+      permanent: false,
+    },
+  };
+}
+
 export default function Home() {
-  return (
-    <div>
-      <h1>ArchaeoRest</h1>
-      <p>Welcome</p>
-      <a href="/map">Go to map</a>
-    </div>
-  );
+  return null;
 }
